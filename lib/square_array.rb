2d90e44c9249f732e81642e296691b65a4e79f05
Christopher Kennedy <chris94kennedy@gmail.com>
2019-07-24 11:46:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -7,3 +7,7 @@ def square_array(array)
   end
   return new_array
 end
+
+#here you used the exponentiation operator (**) followed by the number you want to exponentiate by (squaring,
+#so 2) and a counter starting at 0 to iterate over an array (passed as an argument) and to add to and return
+#a new array containing squared elements of the first array
